756d9b211048613ec61d288671c98e7c7d1d0823
Rangi <remy.oukaour+rangi42@gmail.com>
2020-04-22 12:42:02 -0400

    Add missing gfx/misc.asm

---
+++ gfx/misc.asm
@@ -0,0 +1,107 @@
+; Back pics
+
+SECTION "HiroBackpic", ROMX
+HiroBackpic::
+INCBIN "gfx/player/hiro_back.2bpp.lz"
+
+SECTION "SylviaBackpic", ROMX
+SylviaBackpic::
+INCBIN "gfx/player/sylvia_back.2bpp.lz"
+
+SECTION "DudeBackpic", ROMX
+DudeBackpic::
+INCBIN "gfx/battle/dude.2bpp.lz"
+
+
+; Trainer card pics
+
+SECTION "HiroCardPic", ROMX
+HiroCardPic::
+INCBIN "gfx/trainer_card/hiro_card.2bpp"
+
+SECTION "SylviaCardPic", ROMX
+SylviaCardPic::
+INCBIN "gfx/trainer_card/sylvia_card.2bpp"
+
+
+; Trainer card
+
+SECTION "CardGFX", ROMX
+CardGFX::
+INCBIN "gfx/trainer_card/trainer_card.2bpp"
+
+SECTION "CardStatusGFX", ROMX
+CardStatusGFX::
+INCBIN "gfx/trainer_card/card_status.2bpp"
+
+SECTION "CardRightCornerGFX", ROMX
+CardRightCornerGFX::
+INCBIN "gfx/trainer_card/card_right_corner.2bpp"
+
+SECTION "LeaderGFX", ROMX
+LeaderGFX::
+INCBIN "gfx/trainer_card/leaders.2bpp"
+
+SECTION "LeaderGFX2", ROMX
+LeaderGFX2::
+INCBIN "gfx/trainer_card/leaders.2bpp"
+
+SECTION "BadgeGFX", ROMX
+BadgeGFX::
+INCBIN "gfx/trainer_card/badges.2bpp"
+
+SECTION "BadgeGFX2", ROMX
+BadgeGFX2::
+INCBIN "gfx/trainer_card/badges.2bpp"
+
+
+; Pack
+
+SECTION "PackMenuGFX", ROMX
+PackMenuGFX::
+INCBIN "gfx/pack/pack_menu.2bpp"
+
+SECTION "PackGFX", ROMX
+PackGFX::
+INCBIN "gfx/pack/pack.2bpp"
+
+SECTION "PackFGFX", ROMX
+PackFGFX::
+INCBIN "gfx/pack/pack_f.2bpp"
+
+
+; Pokégear / Town Map
+
+SECTION "PokegearGFX", ROMX
+PokegearGFX::
+INCBIN "gfx/pokegear/pokegear.2bpp.lz"
+
+SECTION "TownMapGFX:", ROMX
+TownMapGFX::
+INCBIN "gfx/pokegear/town_map.2bpp.lz"
+
+
+; Intro / Credits
+
+SECTION "CopyrightGFX:", ROMX
+CopyrightGFX::
+INCBIN "gfx/splash/copyright.2bpp"
+
+SECTION "TheEndGFX:", ROMX
+TheEndGFX::
+INCBIN "gfx/credits/theend.2bpp"
+
+SECTION "Shrink1Pic", ROMX
+Shrink1Pic::
+INCBIN "gfx/new_game/shrink1.2bpp.lz"
+
+SECTION "Shrink2Pic", ROMX
+Shrink2Pic::
+INCBIN "gfx/new_game/shrink2.2bpp.lz"
+
+
+; Unown font
+
+SECTION "UnownFont", ROMX
+UnownFont::
+INCBIN "gfx/font/unown_font.2bpp"
