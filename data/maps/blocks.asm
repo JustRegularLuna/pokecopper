00a5dafe467ca2c61950c318d36d4c9c803d8f94
Rangi <remy.oukaour+rangi42@gmail.com>
2020-04-22 11:34:12 -0400

    Pack map data into individual floating sections

--- data/maps/blocks.asm
+++ data/maps/blocks.asm
@@ -1,47 +1,52 @@
-SECTION "Map Blocks 1", ROMX
-
+SECTION "Pokecenter2F_Blocks", ROMX
 Pokecenter2F_Blocks:
-	INCBIN "maps/Pokecenter2F.blk"
+INCBIN "maps/Pokecenter2F.blk"
 
+SECTION "TradeCenter_Blocks", ROMX
 TradeCenter_Blocks:
 TimeCapsule_Blocks:
-	INCBIN "maps/TradeCenter.blk"
+INCBIN "maps/TradeCenter.blk"
 
+SECTION "Colosseum_Blocks", ROMX
 Colosseum_Blocks:
-	INCBIN "maps/Colosseum.blk"
+INCBIN "maps/Colosseum.blk"
 
-
-SECTION "Map Blocks 2", ROMX
-
+SECTION "SilentHills_Blocks", ROMX
 SilentHills_Blocks:
-	INCBIN "maps/SilentHills.blk"
+INCBIN "maps/SilentHills.blk"
 
+SECTION "PlayersHouse1F_Blocks", ROMX
 PlayersHouse1F_Blocks:
-	INCBIN "maps/PlayersHouse1F.blk"
+INCBIN "maps/PlayersHouse1F.blk"
 
+SECTION "PlayersHouse2F_Blocks", ROMX
 PlayersHouse2F_Blocks:
-	INCBIN "maps/PlayersHouse2F.blk"
+INCBIN "maps/PlayersHouse2F.blk"
 
-
-SECTION "Map Blocks 3", ROMX
-
+SECTION "IndigoPlateauPokecenter1F_Blocks", ROMX
 IndigoPlateauPokecenter1F_Blocks:
-	INCBIN "maps/IndigoPlateauPokecenter1F.blk"
+INCBIN "maps/IndigoPlateauPokecenter1F.blk"
 
+SECTION "WillsRoom_Blocks", ROMX
 WillsRoom_Blocks:
-	INCBIN "maps/WillsRoom.blk"
+INCBIN "maps/WillsRoom.blk"
 
+SECTION "KogasRoom_Blocks", ROMX
 KogasRoom_Blocks:
-	INCBIN "maps/KogasRoom.blk"
+INCBIN "maps/KogasRoom.blk"
 
+SECTION "BrunosRoom_Blocks", ROMX
 BrunosRoom_Blocks:
-	INCBIN "maps/BrunosRoom.blk"
+INCBIN "maps/BrunosRoom.blk"
 
+SECTION "KarensRoom_Blocks", ROMX
 KarensRoom_Blocks:
-	INCBIN "maps/KarensRoom.blk"
+INCBIN "maps/KarensRoom.blk"
 
+SECTION "LancesRoom_Blocks", ROMX
 LancesRoom_Blocks:
-	INCBIN "maps/LancesRoom.blk"
+INCBIN "maps/LancesRoom.blk"
 
+SECTION "HallOfFame_Blocks", ROMX
 HallOfFame_Blocks:
-	INCBIN "maps/HallOfFame.blk"
+INCBIN "maps/HallOfFame.blk"
--- data/maps/scripts.asm
+++ data/maps/scripts.asm
@@ -1,4 +1,4 @@
-SECTION "Map Scripts 1", ROMX
+SECTION "Map Scripts - Cable Club", ROMX
 
 INCLUDE "maps/Pokecenter2F.asm"
 INCLUDE "maps/TradeCenter.asm"
@@ -6,14 +6,14 @@ INCLUDE "maps/Colosseum.asm"
 INCLUDE "maps/TimeCapsule.asm"
 
 
-SECTION "Map Scripts 2", ROMX
+SECTION "Map Scripts - Silent Hills", ROMX
 
 INCLUDE "maps/SilentHills.asm"
 INCLUDE "maps/PlayersHouse1F.asm"
 INCLUDE "maps/PlayersHouse2F.asm"
 
 
-SECTION "Map Scripts 3", ROMX
+SECTION "Map Scripts - Indigo Plateau", ROMX
 
 INCLUDE "maps/IndigoPlateauPokecenter1F.asm"
 INCLUDE "maps/WillsRoom.asm"
@@ -22,69 +22,3 @@ INCLUDE "maps/BrunosRoom.asm"
 INCLUDE "maps/KarensRoom.asm"
 INCLUDE "maps/LancesRoom.asm"
 INCLUDE "maps/HallOfFame.asm"
-
-
-SECTION "Map Scripts 4", ROMX
-
-
-SECTION "Map Scripts 5", ROMX
-
-
-SECTION "Map Scripts 6", ROMX
-
-
-SECTION "Map Scripts 7", ROMX
-
-
-SECTION "Map Scripts 8", ROMX
-
-
-SECTION "Map Scripts 9", ROMX
-
-
-SECTION "Map Scripts 10", ROMX
-
-
-SECTION "Map Scripts 11", ROMX
-
-
-SECTION "Map Scripts 12", ROMX
-
-
-SECTION "Map Scripts 13", ROMX
-
-
-SECTION "Map Scripts 14", ROMX
-
-
-SECTION "Map Scripts 15", ROMX
-
-
-SECTION "Map Scripts 16", ROMX
-
-
-SECTION "Map Scripts 17", ROMX
-
-
-SECTION "Map Scripts 18", ROMX
-
-
-SECTION "Map Scripts 19", ROMX
-
-
-SECTION "Map Scripts 20", ROMX
-
-
-SECTION "Map Scripts 21", ROMX
-
-
-SECTION "Map Scripts 22", ROMX
-
-
-SECTION "Map Scripts 23", ROMX
-
-
-SECTION "Map Scripts 24", ROMX
-
-
-SECTION "Map Scripts 25", ROMX
